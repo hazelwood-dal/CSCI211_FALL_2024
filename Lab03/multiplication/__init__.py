d36d203705108d7e093f9e87ce1846b80d68f0e3
hazelwood-dal <73398226+hazelwood-dal@users.noreply.github.com>
2024-09-10 11:10:29 -0400

    test commit

--- Lab03/multiplication/__init__.py
+++ Lab03/multiplication/__init__.py
@@ -6,17 +6,17 @@ def exists():
     """multiplication.py exists"""
     check50.exists("multiplication.py")
 
-@check50.check()
+@check50.check(exists)
 def test_5x5():
     """Correctly prints a 5x5 multiplication table"""
     check50.run("python3 multiplication.py").stdin("5").stdout("  1  2  3  4  5\n  2  4  6  8 10\n  3  6  9 12 15\n  4  8 12 16 20\n  5 10 15 20 25\n", regex=False).exit(0)
 
-@check50.check()
+@check50.check(exists)
 def test_3x3():
     """Correctly prints a 3x3 multiplication table"""
     check50.run("python3 multiplication.py").stdin("3").stdout("  1  2  3\n  2  4  6\n  3  6  9\n", regex=False).exit(0)
 
-@check50.check()
+@check50.check(exists)
 def test_1x1():
     """Correctly prints a 1x1 multiplication table"""
     check50.run("python3 multiplication.py").stdin("1").stdout("  1\n", regex=False).exit(0)
